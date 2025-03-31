dc95401f98e081ed0fc21216dc3c00bcc555079c
N1A1voIt <120253454+N1A1voIt@users.noreply.github.com>
2025-03-31 18:40:19 +0300

    [feat]-Create group (Front end only)

--- frontend/src/app/group/create-group/create-group.page.ts
+++ frontend/src/app/group/create-group/create-group.page.ts
@@ -1,29 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {IonContent, IonHeader, IonIcon, IonTitle, IonToolbar} from '@ionic/angular/standalone';
 import {Observable} from "rxjs";
 import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import {addIcons} from "ionicons";
+import {addCircleOutline, arrowBack} from "ionicons/icons";
+export interface Users {
+  username:string,
+  email:string,
+  photo:string,
+}
 @Component({
   selector: 'app-create-group',
   templateUrl: './create-group.page.html',
   styleUrls: ['./create-group.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonIcon]
 })
 export class CreateGroupPage implements OnInit {
-
-  constructor(private firestore:Firestore) { }
+  users:Users[] = [];
+  showButtons:boolean = false;
+  constructor(private firestore:Firestore) {
+    addIcons({
+      arrowBack,
+      addCircleOutline
+    })
+  }
 
   ngOnInit() {
     this.getUsers().subscribe({
       next: data => {
+        this.users = <Users[]>data;
         console.log(data);
       },error:error => {
         console.log(error);
       }
     })
   }
+
+
 
   getUsers() {
     const usersRef = collection(this.firestore, 'users');
