049976f6335abbc98c075756fd3293475c07b7bc
N1A1voIt <120253454+N1A1voIt@users.noreply.github.com>
2025-03-31 17:07:46 +0300

    Kapi intro fix

--- frontend/src/app/intro/intro.page.ts
+++ frontend/src/app/intro/intro.page.ts
@@ -4,13 +4,15 @@ import { FormsModule } from '@angular/forms';
 import {IonContent, IonHeader, IonIcon, IonTitle, IonToolbar} from '@ionic/angular/standalone';
 import {addIcons} from "ionicons";
 import {arrowForwardOutline} from "ionicons/icons";
+import {KapiHeaderComponent} from "../kapi/header/kapi.header.component";
+import {KapiComponent} from "../kapi/kapi.component";
 
 @Component({
   selector: 'app-intro',
   templateUrl: './intro.page.html',
   styleUrls: ['./intro.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonIcon]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonIcon, KapiHeaderComponent, KapiComponent]
 })
 export class IntroPage implements OnInit {
 
