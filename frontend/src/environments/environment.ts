266aefb83becfbabac184a33dcfcb2aa5e238656
N1A1voIt <120253454+N1A1voIt@users.noreply.github.com>
2025-03-31 11:46:34 +0300

    [inti]-Firebase init

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyA88bAvpNatzYW4dRPsvzjNiSjhRfjo_sM",
+    authDomain: "hiu-selection.firebaseapp.com",
+    projectId: "hiu-selection",
+    storageBucket: "hiu-selection.firebasestorage.app",
+    messagingSenderId: "1004834507721",
+    appId: "1:1004834507721:web:a7ddf47b651d03ae03f618",
+    measurementId: "G-CDYKGX66XT"
+  }
 };
 
 /*
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,14 +1,29 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import {bootstrapApplication, provideClientHydration} from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import {environment} from "./environments/environment";
+import {importProvidersFrom} from "@angular/core";
+import {IonicModule} from "@ionic/angular";
+import {FIREBASE_OPTIONS} from "@angular/fire/compat";
+import {provideFirebaseApp} from "@angular/fire/app";
+import firebase from "firebase/compat";
+import initializeApp = firebase.initializeApp;
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
+    { provide: FIREBASE_OPTIONS, useValue: environment.firebaseConfig },
+    importProvidersFrom(IonicModule.forRoot()),
+    provideClientHydration(),
+    provideRouter(routes),
+    provideFirebaseApp(() => {
+      console.log("Initializing Firebase...");
+      return initializeApp(environment.firebaseConfig);
+    }),
   ],
 });
